3e35cd75d0295eebcded1c0f835e3ed0c272ebec
dericward <dericward@hotmail.com>
2023-02-04 12:56:22 +0000

    changed ajax inner function to include an object of items from api response, now have a function outside of ajax that recieves the api response data

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,25 +7,52 @@
         searchCity = $("#search-input").val();
         queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + searchCity + "&mode=json&units=metric&appid=" + APIKey;
         // console.log(queryURL);
-    
+
         $.ajax({
             url: queryURL,
             method: "GET"
-        }).then(function(response) {
-            // console.log(response);
-            let city = response.name;
-            console.log(city);
-            let temp = response.main.temp;
-            console.log(temp);
-            let humidity = response.main.humidity;
-            console.log(humidity);
-            let windSpeed = response.wind.speed;
-            console.log(windSpeed);
-            let latitude = response.coord.lat;
-            console.log(latitude);
-            let longtitude = response.coord.lon;
-            console.log(longtitude);
-        });
+            }).then(function(response) {
+                let APIResponse = {
+                    city: response.name,
+                    temp: response.main.temp,
+                    humidity: response.main.humidity,
+                    windSpeed: response.wind.speed,
+                    latitude: response.coord.lat,
+                    longtitude: response.coord.lon
+                };
+            console.log(APIResponse.city);
+            APIData(APIResponse);        
+        }); 
     });
 
+    function APIData(APIResponse) {
+        console.log("City: " + APIResponse.city)
+    }
 
+
+
+    // console.log("city: " + APIResponse.city); // return APIResponse is not defined
+
+
+// console.log(response);
+
+
+    //     $.ajax({
+    //         url: queryURL,
+    //         method: "GET"
+    //     }).then(function(response) {
+    //         // console.log(response);
+    //         let city = response.name;
+    //         console.log(city);
+    //         let temp = response.main.temp;
+    //         console.log(temp);
+    //         let humidity = response.main.humidity;
+    //         console.log(humidity);
+    //         let windSpeed = response.wind.speed;
+    //         console.log(windSpeed);
+    //         let latitude = response.coord.lat;
+    //         console.log(latitude);
+    //         let longtitude = response.coord.lon;
+    //         console.log(longtitude);
+    //     });
+    // });
