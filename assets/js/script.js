41270a79b61aac8d5bf36b01aceb07cf23242e9f
dericward <dericward@hotmail.com>
2023-02-05 18:57:10 +0000

    added fetched API data to the 'today' display area

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,18 @@
     const APIKey = "451e60a05a73232b3bf03933f49433c3";
 
+    let cityDisplay = document.getElementById("city");
+    let todayDateDisplay = document.getElementById("today-date");
+    let todayTempDisplay = document.getElementById("today-temp");
+    let todayHumidityDisplay = document.getElementById("today-humidity");
+    let todayWindSpeedDisplay = document.getElementById("today-wind-speed");
+
+    let todaysDate = moment();
     let searchCity = "";
     let queryURL = "";
     
     $("#search-button").on("click", function() {
         searchCity = $("#search-input").val();
         queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + searchCity + "&mode=json&units=metric&appid=" + APIKey;
-        // console.log(queryURL);
 
         $.ajax({
             url: queryURL,
@@ -19,15 +25,17 @@
                     windSpeed: response.wind.speed,
                     lat: response.coord.lat,
                     lon: response.coord.lon
-                };
-            console.log(APIResponse);
-            console.log("Inside AJAX: ", APIResponse);
-            APIData(APIResponse);        
+                };  
+            displayWeather(APIResponse);  
         }); 
     });
 
-    function APIData(APIResponse) {
-        console.log("outside AJAX:", APIResponse);
+    function displayWeather(APIResponse) {
+        cityDisplay.textContent = APIResponse.city + " " +  "(" + todaysDate.format("DD/MM/YYYY") + ")";
+        todayTempDisplay.textContent = "Temp: " + APIResponse.temp + " Degrees C";
+        todayHumidityDisplay.textContent = "Humidity: " + APIResponse.humidity + "%";
+        todayWindSpeedDisplay.textContent = "Wind: " + APIResponse.windSpeed + " m/s";
+
         console.log("City: ", APIResponse.city);
         console.log("Temp: ", APIResponse.temp);
         console.log("Humidity: ", APIResponse.humidity);
@@ -35,31 +43,3 @@
         console.log("Lat : ", APIResponse.lat);
         console.log("Lon : ", APIResponse.lon);
     };
-
-
-
-    // console.log("city: " + APIResponse.city); // return APIResponse is not defined
-
-
-// console.log(response);
-
-
-    //     $.ajax({
-    //         url: queryURL,
-    //         method: "GET"
-    //     }).then(function(response) {
-    //         // console.log(response);
-    //         let city = response.name;
-    //         console.log(city);
-    //         let temp = response.main.temp;
-    //         console.log(temp);
-    //         let humidity = response.main.humidity;
-    //         console.log(humidity);
-    //         let windSpeed = response.wind.speed;
-    //         console.log(windSpeed);
-    //         let latitude = response.coord.lat;
-    //         console.log(latitude);
-    //         let longtitude = response.coord.lon;
-    //         console.log(longtitude);
-    //     });
-    // });
