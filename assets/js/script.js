846b3f334807e294de6fee5599f27f718a68cae2
dericward <dericward@hotmail.com>
2023-02-06 00:25:37 +0000

    working on a more DRY and streamlined approach to getting, storing and displaying the data using an array of obejects

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,38 +8,62 @@
 
     let todaysDate = moment();
     let searchCity = "";
+    let queryLatLon = "";
     let queryURL = "";
-    
-    $("#search-button").on("click", function() {
-        searchCity = $("#search-input").val();
-        queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + searchCity + "&mode=json&units=metric&appid=" + APIKey;
 
+    function displayToday(response) {
+        cityDisplay.textContent = response.city + " " +  "(" + todaysDate.format("DD/MM/YYYY") + ")";
+        todayTempDisplay.textContent = `Temp: ${response.temp} Degrees C`;
+        todayHumidityDisplay.textContent = `Humidity: ${response.humidity}%`;
+        todayWindSpeedDisplay.textContent = `Wind: ${response.windSpeed} m/s`;
+    };
+
+    function display5Day(response) {
+        queryURL = `https://api.openweathermap.org/data/2.5/forecast?lat=${response.lat}&lon=${response.lon}&appid=${APIKey}`;
+    
         $.ajax({
             url: queryURL,
             method: "GET"
             }).then(function(response) {
-                let APIResponse = {
+                let fiveDayArray = [];
+                console.log(response);
+                for(let i = 4; i < 37; i+= 8) {
+                    let day = {
+                    temp: response.list[i].main.temp,
+                    wind: response.list[i].wind.speed,
+                    humidity: response.list[i].main.humidity
+                    }
+                    fiveDayArray.push(day);
+                    // console.log(temp); 
+                    // console.log(wind);
+                    // console.log(humidity + "%");
+                };
+                console.log(fiveDayArray);
+            
+        });
+    };
+    
+    $("#search-button").on("click", function() {
+        searchCity = $("#search-input").val();
+        queryLatLon = `https://api.openweathermap.org/data/2.5/weather?q=${searchCity}&mode=json&units=metric&appid=${APIKey}`;    
+
+        $.ajax({
+            url: queryLatLon,
+            method: "GET"
+            }).then(function(response) {
+                let todayAndLatLon = {
                     city: response.name,
                     temp: response.main.temp,
                     humidity: response.main.humidity,
                     windSpeed: response.wind.speed,
                     lat: response.coord.lat,
                     lon: response.coord.lon
-                };  
-            displayWeather(APIResponse);  
-        }); 
+                };
+                displayToday(todayAndLatLon)
+                display5Day(todayAndLatLon); 
+            });
     });
 
-    function displayWeather(APIResponse) {
-        cityDisplay.textContent = APIResponse.city + " " +  "(" + todaysDate.format("DD/MM/YYYY") + ")";
-        todayTempDisplay.textContent = "Temp: " + APIResponse.temp + " Degrees C";
-        todayHumidityDisplay.textContent = "Humidity: " + APIResponse.humidity + "%";
-        todayWindSpeedDisplay.textContent = "Wind: " + APIResponse.windSpeed + " m/s";
 
-        console.log("City: ", APIResponse.city);
-        console.log("Temp: ", APIResponse.temp);
-        console.log("Humidity: ", APIResponse.humidity);
-        console.log("Wind speed: ", APIResponse.windSpeed);
-        console.log("Lat : ", APIResponse.lat);
-        console.log("Lon : ", APIResponse.lon);
-    };
+
+
