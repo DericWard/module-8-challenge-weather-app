4ae9eeb63638f72a4709a01a578fde68553fd58c
dericward <dericward@hotmail.com>
2023-02-06 11:52:33 +0000

    got incrementing dates on day cards working

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,33 +6,40 @@
     let todayHumidityDisplay = document.getElementById("today-humidity");
     let todayWindSpeedDisplay = document.getElementById("today-wind-speed");
     
-
-    let card0Temp = document.getElementById("card0"); // tomorrow
-    let card0WindSpeed = document.getElementById("card0"); // tomorrow
-    let card0Humidity = document.getElementById("card0"); // tomorrow
-
-    let card1Temp = document.getElementById("card1-temp"); // day after tomorrow
-    let card1WindSpeed = document.getElementById("card1-wind"); // day after tomorrow
-    let card1Humidity = document.getElementById("card1-humidity"); // day after tomorrow
+    // start card display fields
+    let card0Temp = document.getElementById("card0"); // today
+    let card0WindSpeed = document.getElementById("card0"); // 
+    let card0Humidity = document.getElementById("card0"); // 
     
-    let card2Temp = document.getElementById("card2-temp"); // etc
+    let card1Date = document.getElementById("card1-date"); // 
+    let card1Temp = document.getElementById("card1-temp"); // tomorrow
+    let card1WindSpeed = document.getElementById("card1-wind"); 
+    let card1Humidity = document.getElementById("card1-humidity"); 
+    
+    let card2Date = document.getElementById("card2-date"); //
+    let card2Temp = document.getElementById("card2-temp"); // day after tomorrow
     let card2WindSpeed = document.getElementById("card2-wind"); 
     let card2Humidity = document.getElementById("card2-humidity"); 
     
+    let card3Date = document.getElementById("card3-date"); //
     let card3Temp = document.getElementById("card3-temp"); // and so on
     let card3WindSpeed = document.getElementById("card3-wind"); 
     let card3Humidity = document.getElementById("card3-humidity"); 
 
+    let card4Date = document.getElementById("card4-date"); //
     let card4Temp = document.getElementById("card4-temp"); 
     let card4WindSpeed = document.getElementById("card4-wind"); 
     let card4Humidity = document.getElementById("card4-humidity"); 
 
+    let card5Date = document.getElementById("card5-date"); //
     let card5Temp = document.getElementById("card5-temp"); 
     let card5WindSpeed = document.getElementById("card5-wind"); 
     let card5Humidity = document.getElementById("card5-humidity"); 
+    // end card display fields
 
-    let K = 273.15;
+    let K = 273.15; // c = K - 273.15 kelvin to centigrade
     let todaysDate = moment();
+    // incrementDate = todaysDate.add(1, 'd').format("DD/M/YYYY");
     let searchCity = "";
     let queryLatLon = "";
     let queryURL = "";
@@ -56,6 +63,7 @@
                 for(let i = 4; i < 37; i+= 8) {
                     let day = {
                     // temp: Math.round(response.list[i].main.temp - K),
+                    date: response.list[i].dt_txt,
                     temp: (response.list[i].main.temp - K).toFixed(2),
                     wind: response.list[i].wind.speed,
                     humidity: response.list[i].main.humidity
@@ -68,24 +76,31 @@
                     // `card[i]Temp.textContent` = fiveDayArray[i].temp;
                 }
 
-                // c = K - 273.15 kelvin to centigrade
+
+                // card1Date.textContent = todaysDate.add(1, 'd').format("DD/M/YYYY");
+                card1Date.textContent = todaysDate.add(1, 'd').format("DD/M/YYYY");
                 card1Temp.textContent = `Temp: ${fiveDayArray[0].temp}\u00B0C`;
+                // card1Temp.textContent = `${fiveDayArray[0].date}`;
                 card1WindSpeed.textContent = `Wind: ${fiveDayArray[0].wind}`;
                 card1Humidity.textContent = `Humidity: ${fiveDayArray[0].humidity}%`;
                 
+                card2Date.textContent = todaysDate.add(1, 'd').format("DD/M/YYYY");
                 card2Temp.textContent = `Temp: ${fiveDayArray[1].temp}\u00B0C`;
                 card2WindSpeed.textContent = `Wind: ${fiveDayArray[1].wind}`;
                 card2Humidity.textContent = `Humidity: ${fiveDayArray[1].humidity}%`;
 
+                card3Date.textContent = todaysDate.add(1, 'd').format("DD/M/YYYY");
                 card3Temp.textContent = `Temp: ${fiveDayArray[2].temp}\u00B0C`;
                 card3WindSpeed.textContent = `Wind: ${fiveDayArray[2].wind}`;
                 card3Humidity.textContent = `Humidity: ${fiveDayArray[2].humidity}%`;
 
+                card4Date.textContent = todaysDate.add(1, 'd').format("DD/M/YYYY");
                 card4Temp.textContent = `Temp: ${fiveDayArray[3].temp}\u00B0C`;
                 card4WindSpeed.textContent = `Wind: ${fiveDayArray[3].wind}`;
                 card4Humidity.textContent = `Humidity: ${fiveDayArray[3].humidity}%`;
 
-                card5Temp.textContent = `Temp ${fiveDayArray[4].temp}\u00B0C`;
+                card5Date.textContent = todaysDate.add(1, 'd').format("DD/M/YYYY");;
+                card5Temp.textContent = `Temp: ${fiveDayArray[4].temp}\u00B0C`;
                 card5WindSpeed.textContent = `Wind: ${fiveDayArray[4].wind}`;
                 card5Humidity.textContent = `Humidity: ${fiveDayArray[4].humidity}%`;
 
@@ -96,7 +111,8 @@
     $("#search-button").on("click", function() {
         searchCity = $("#search-input").val();
         queryLatLon = `https://api.openweathermap.org/data/2.5/weather?q=${searchCity}&mode=json&units=metric&appid=${APIKey}`;    
-
+        // console.log(todaysDate.add(1, 'd').format("DD/MM/YYYY"));
+        // console.log(moment(todaysDate, "DD/MM/YYYY"));
         $.ajax({
             url: queryLatLon,
             method: "GET"
