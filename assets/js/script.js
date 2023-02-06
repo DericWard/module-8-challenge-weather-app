680a71df1e4f40d5908d3adbd45d3ede098a6469
dericward <dericward@hotmail.com>
2023-02-06 01:50:41 +0000

    all cards now updating with stats, kelvin converted to celsius, need to add date to DAYn

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,7 +5,33 @@
     let todayTempDisplay = document.getElementById("today-temp");
     let todayHumidityDisplay = document.getElementById("today-humidity");
     let todayWindSpeedDisplay = document.getElementById("today-wind-speed");
+    
 
+    let card0Temp = document.getElementById("card0"); // tomorrow
+    let card0WindSpeed = document.getElementById("card0"); // tomorrow
+    let card0Humidity = document.getElementById("card0"); // tomorrow
+
+    let card1Temp = document.getElementById("card1-temp"); // day after tomorrow
+    let card1WindSpeed = document.getElementById("card1-wind"); // day after tomorrow
+    let card1Humidity = document.getElementById("card1-humidity"); // day after tomorrow
+    
+    let card2Temp = document.getElementById("card2-temp"); // etc
+    let card2WindSpeed = document.getElementById("card2-wind"); 
+    let card2Humidity = document.getElementById("card2-humidity"); 
+    
+    let card3Temp = document.getElementById("card3-temp"); // and so on
+    let card3WindSpeed = document.getElementById("card3-wind"); 
+    let card3Humidity = document.getElementById("card3-humidity"); 
+
+    let card4Temp = document.getElementById("card4-temp"); 
+    let card4WindSpeed = document.getElementById("card4-wind"); 
+    let card4Humidity = document.getElementById("card4-humidity"); 
+
+    let card5Temp = document.getElementById("card5-temp"); 
+    let card5WindSpeed = document.getElementById("card5-wind"); 
+    let card5Humidity = document.getElementById("card5-humidity"); 
+
+    let K = 273.15;
     let todaysDate = moment();
     let searchCity = "";
     let queryLatLon = "";
@@ -29,17 +55,41 @@
                 console.log(response);
                 for(let i = 4; i < 37; i+= 8) {
                     let day = {
-                    temp: response.list[i].main.temp,
+                    // temp: Math.round(response.list[i].main.temp - K),
+                    temp: (response.list[i].main.temp - K).toFixed(2),
                     wind: response.list[i].wind.speed,
                     humidity: response.list[i].main.humidity
                     }
                     fiveDayArray.push(day);
-                    // console.log(temp); 
-                    // console.log(wind);
-                    // console.log(humidity + "%");
                 };
-                console.log(fiveDayArray);
-            
+                for (i = 1; i < 6; i++) {
+                    //update each card's data fields, i is the card number
+                    console.log("card" + [i]);
+                    // `card[i]Temp.textContent` = fiveDayArray[i].temp;
+                }
+
+                // c = K - 273.15 kelvin to centigrade
+                card1Temp.textContent = `Temp: ${fiveDayArray[0].temp}`;
+                card1WindSpeed.textContent = `Wind: ${fiveDayArray[0].wind}`;
+                card1Humidity.textContent = `Humidity: ${fiveDayArray[0].humidity}%`;
+                
+                card2Temp.textContent = `Temp: ${fiveDayArray[1].temp}`;
+                card2WindSpeed.textContent = `Wind: ${fiveDayArray[1].wind}`;
+                card2Humidity.textContent = `Humidity: ${fiveDayArray[1].humidity}%`;
+
+                card3Temp.textContent = `Temp: ${fiveDayArray[2].temp}`;
+                card3WindSpeed.textContent = `Wind: ${fiveDayArray[2].wind}`;
+                card3Humidity.textContent = `Humidity: ${fiveDayArray[2].humidity}%`;
+
+                card4Temp.textContent = `Temp: ${fiveDayArray[3].temp}`;
+                card4WindSpeed.textContent = `Wind: ${fiveDayArray[3].wind}`;
+                card4Humidity.textContent = `Humidity: ${fiveDayArray[3].humidity}%`;
+
+                card5Temp.textContent = `Temp ${fiveDayArray[4].temp}`;
+                card5WindSpeed.textContent = `Wind: ${fiveDayArray[4].wind}`;
+                card5Humidity.textContent = `Humidity: ${fiveDayArray[4].humidity}%`;
+
+                console.log(fiveDayArray);      
         });
     };
     
