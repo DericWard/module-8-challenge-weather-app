2d49901af1994a3fe5185256e263a57086f3e732
dericward <dericward@hotmail.com>
2023-02-21 22:35:45 +0000

    removed time of day of forecast, Math.ceil to temp and wind speed values, improved mobile look

--- assets/js/script2.js
+++ assets/js/script2.js
@@ -15,29 +15,29 @@ function display5Day(response) {
         }).then(function(response) {
             let fiveDayArray = [];
             let K = 273.15; // c = K - 273.15 (kelvin to centigrade conversion)
-            
-            for(let i = 0; i < 33; i+= 8) {
+            for(let i = 0; i < 37; i+= 8) {
                 let day = {
                 date: response.list[i].dt_txt,
                 icon: response.list[i].weather[0].icon,
-                temp: (response.list[i].main.temp - K).toFixed(2),
-                wind: response.list[i].wind.speed,
+                // temp: (response.list[i].main.temp - K).toFixed(2),
+                temp: Math.ceil(response.list[i].main.temp - K),
+                wind: Math.ceil(response.list[i].wind.speed),
                 humidity: response.list[i].main.humidity
                 };
 
                 let dateYear = day.date.slice(0, 4);
                 let dateMonth = day.date.slice(5, 7);
                 let dateDay = day.date.slice(8, 10);
-                let dayTime = day.date.slice(11, 16);
+                // let dayTime = day.date.slice(11, 16);
 
-                day.time = `${dayTime}`;             
+                // day.time = `${dayTime}`;             
                 day.date = `${dateDay}/${dateMonth}/${dateYear}`;
                 fiveDayArray.push(day);   
             };
             
             for (let i = 0; i < 5; i++) {
                 let cardDate = document.getElementById(`card${i+1}-date`);
-                let cardTime = document.getElementById(`card${i+1}-time`);
+                // let cardTime = document.getElementById(`card${i+1}-time`);
                 let cardTemp = document.getElementById(`card${i+1}-temp`);
                 let cardWind = document.getElementById(`card${i+1}-wind`);
                 let cardHumidity = document.getElementById(`card${i+1}-humidity`);
@@ -46,7 +46,7 @@ function display5Day(response) {
                 document.querySelector('#card' + (i+1) + '-icon').setAttribute('src', cardIconURL);
 
                 cardDate.textContent = `(${fiveDayArray[i].date})`;// increment date for each card
-                cardTime.textContent = `@${fiveDayArray[i].time}`;    
+                // cardTime.textContent = `@${fiveDayArray[i].time}`;    
                 cardTemp.textContent = `Temp: ${fiveDayArray[i].temp}\u00B0C`;
                 cardWind.textContent = `Wind: ${fiveDayArray[i].wind} m/sec`;
                 cardHumidity.textContent = `Humidity: ${fiveDayArray[i].humidity}%`;                     
@@ -56,7 +56,6 @@ function display5Day(response) {
 
 // un-hide the display for today and use the API data to write to display today's current forecast
 function displayToday(response) {  
-    let todaysDate = moment();
     let todaySection = document.getElementById("today-section");
     let cityDisplay = document.getElementById("city");
     let todayTempDisplay = document.getElementById("today-temp");
@@ -69,10 +68,11 @@ function displayToday(response) {
     todaySection.style.display = "flex";
     $(todaySection).show();
 
-    cityDisplay.textContent = `${response.city} (${todaysDate.format("DD/MM/YYYY")})`;
-
-    todayTempDisplay.textContent = `\u00A0\u00A0Temp: ${response.temp}\u00B0C\u00A0\u00A0\u00A0`;
-    todayWindSpeedDisplay.textContent = `Wind: ${response.windSpeed} m/s\u00A0\u00A0\u00A0`;
+    cityDisplay.textContent = `${response.city}: `;
+    temp = Math.ceil(response.temp);
+    wind = Math.ceil(response.windSpeed);
+    todayTempDisplay.textContent = `Temp: ${temp}\u00B0C\u00A0\u00A0\u00A0`;
+    todayWindSpeedDisplay.textContent = `Wind: ${wind}m/s\u00A0\u00A0\u00A0`;
     todayHumidityDisplay.textContent = `Humidity: ${response.humidity}%`;
 
     display5Day(response);
@@ -122,8 +122,6 @@ function getLatAndLon(searchCity) {
         url: queryLatLon,
         method: "GET"
         }).then(function(response) {
-            console.log(response);
-            console.log(response.weather[0].icon);
             let todayAndLatLon = {
                 city: response.name,
                 icon: response.weather[0].icon, 
